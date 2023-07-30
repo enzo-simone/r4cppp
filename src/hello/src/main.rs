a36c4e7b64a80babb64c68f82314c250a6d217b4
Enzo Simone <enzo.simone.dev@gmail.com>
2023-07-30 20:52:13 +0200

    'if' is a statement which returns

--- src/hello/src/main.rs
+++ src/hello/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let string = "Hello, world!";
+    println!("{}", string);
+
+    let max_lenght = 4;
+
+    println!(
+        "String \"{}\" is {} than {} characters.",
+        string,
+        if max_lenght < 10 {
+            "shorter"
+        } else {
+            "not shorter"
+        },
+        max_lenght
+    );
 }
